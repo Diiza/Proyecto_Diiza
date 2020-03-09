edab1f6858c7e091b45c263d426f1fe254e965b2
Kary-Garcia <60681687+Kary-Garcia@users.noreply.github.com>
2020-03-09 11:59:15 -0600

    Beta 1.1.1

---
+++ Beta1.0/Assets/Scripts/Control_Botones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Control_Botones : MonoBehaviour
+{
+    public void MenuInicio() {
+        SceneManager.LoadScene("Menu_Principal");
+    }
+}
